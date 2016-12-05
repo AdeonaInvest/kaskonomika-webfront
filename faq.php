36ccce9c4f2e65f243678c0a43a9fe9c65bf9880
leo_g <lg@kaskonomika.ru>
2016-12-05 13:13:07 +0300

    + GTM на страницах faq, howItWork

--- faq.php
+++ faq.php
@@ -4,6 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5KX8PGH');</script>
+    <!-- End Google Tag Manager -->
+
     <!--google+-->
     <meta itemprop="name" content="КАСКОНОМИКА"/>
     <meta itemprop="description" content="КАСКОНОМИКА"/>
@@ -68,6 +76,11 @@
 </head>
 
 <body style="overflow-y: hidden;">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KX8PGH"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
 <div class="header">
     <ul id="toolbar" class="toolbar">
         <li id="menu" class="menu"><div class="menuIconLine"></div><div class="menuIconLine"></div><div class="menuIconLine"></div></li>
--- howItWork.php
+++ howItWork.php
@@ -5,6 +5,14 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
+        <!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-5KX8PGH');</script>
+		<!-- End Google Tag Manager -->
+
         <!--google+-->
         <meta itemprop="name" content="КАСКОНОМ??КА"/>
         <meta itemprop="description" content="КАСКОНОМ??КА"/>
@@ -67,6 +75,10 @@
         <title>Как это работает?</title>
     </head>
     <body>
+        <!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KX8PGH"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
 
 
     <style>
