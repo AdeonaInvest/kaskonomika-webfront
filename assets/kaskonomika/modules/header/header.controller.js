05f07fd8fe8bbfa4d650eb905ed65f00adbf53cc
Ravy <smart.fz@mail.ru>
2017-05-23 19:25:02 +0300

    Footer + много всякого

--- assets/kaskonomika/modules/header/header.controller.js
+++ assets/kaskonomika/modules/header/header.controller.js
@@ -9,6 +9,43 @@
     function headerController() {
         var vm = this;
         vm.openOverlay = false;
+        vm.mainMenu = [
+            {
+                text: 'Умное страхование',
+                url: '/'
+            },
+            {
+                text: 'Как это работает',
+                url: '/how-it-work'
+            },
+            {
+                text: 'Вопросы и ответы',
+                url: '/faq'
+            },
+            {
+                text: 'Скачать приложение',
+                url: '/download'
+            }
+        ];
+
+        vm.subMenu = [
+            {
+                text: 'Реквизиты',
+                url: '/requisites'
+            },
+            {
+                text: 'Оплата и доставка',
+                url: '/shipping'
+            },
+            {
+                text: 'Политика конфиденциальности',
+                url: '/privacy_policy'
+            },
+            {
+                text: 'Контакты',
+                url: '/contacts'
+            }
+        ];
 
         activate();
         /////////////////////
