42855a44f18034822e3dff88421da3a7a08a2ca1
Ravy <smart.fz@mail.ru>
2017-06-22 19:04:39 +0300

    faq

--- assets/kaskonomika/modules/pages/faq/faq.controller.js
+++ assets/kaskonomika/modules/pages/faq/faq.controller.js
@@ -5,9 +5,9 @@
         .module('kaskonomika')
         .controller('faqController', faqController);
 
-    faqController.$inject = ['$scope'];
+    faqController.$inject = ['$scope','$http'];
 
-    function faqController($scope) {
+    function faqController($scope,$http) {
         ///////////////////
         var vm = this;
         vm.view = false; //Статус готовности отображения
@@ -16,8 +16,20 @@
             $scope.$on('cfpLoadingBar:completed',function(){
                 vm.view = true;
             });
+            getQuestions(); //Получение вопросов
         }
         //////////////////
+        vm.accordeonClose = true;
+
+        /**
+         * Получение вопросов из JSON файла. Расположение: /src/common/json
+         */
+        function getQuestions() {
+            $http.get('https://sitelaravel.kaskonomika.ru/faq/section/list')
+                .then(function(response){
+                    vm.questionsResp = response.data;
+                })
+        }
         
     }
 })();
