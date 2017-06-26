8f4631de565482e5768afd6d7e388996027f02d4
Ravy <smart.fz@mail.ru>
2017-06-26 13:38:07 +0300

    Downloads

--- assets/kaskonomika/modules/pages/download/download.controller.js
+++ assets/kaskonomika/modules/pages/download/download.controller.js
@@ -5,9 +5,9 @@
         .module('kaskonomika')
         .controller('downloadController', downloadController);
 
-    downloadController.$inject = ['$scope'];
+    downloadController.$inject = ['$scope','$interval'];
 
-    function downloadController($scope) {
+    function downloadController($scope,$interval) {
         ///////////////////
         var vm = this;
         vm.view = false; //Статус готовности отображения
@@ -16,8 +16,32 @@
             $scope.$on('cfpLoadingBar:completed',function(){
                 vm.view = true;
             });
+
+            phoneSlider(); //Запуск слайдера
         }
         //////////////////
+        vm.phone = 1; // Начальная позиция слайдера
+
+        vm.clearIntervalSlider = clearIntervalSlider; //Отключение слайдера при клике на стрелочки
+
+        /**
+         * Запуск слайдера
+         */
+        function phoneSlider(){
+            vm.interval = $interval(function(){
+                if (vm.phone > 2) {
+                    vm.phone = 0;
+                }
+                vm.phone++;
+            },3000);
+        }
+
+        /**
+         * Отключение слайдера при клике на стрелочки
+         */
+        function clearIntervalSlider(){
+            $interval.cancel(vm.interval)
+        }
         
     }
 })();
