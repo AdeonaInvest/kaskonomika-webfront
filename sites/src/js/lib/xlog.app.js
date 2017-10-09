7852a94cb5eba06c6f4eeb7772d04c66f81750d6
Ravy <smart.fz@mail.ru>
2017-10-09 15:58:02 +0300

    Добрался до dashboard. Insurance List

--- sites/src/js/lib/xlog.app.js
+++ sites/src/js/lib/xlog.app.js
@@ -3,13 +3,32 @@
  * Вывод в консоль необходимой инфы для отладки. На проде этой информации видно не будет
  * xlog('same text', vm.data)
  */
-var xlog = function(text, data) {
+let xlog = function(text, data) {
     if (window.location.host.indexOf('local') !== -1) {
         /*var list = '';
         for (var i=0; i < arguments.length; i++) {
             list = list + arguments[i] + ' ';
         }*/
         console.info(text,data || '');
+    }
+};
+
+let xerror = function(text, data) {
+    if (window.location.host.indexOf('local') !== -1) {
+        /*var list = '';
+        for (var i=0; i < arguments.length; i++) {
+            list = list + arguments[i] + ' ';
+        }*/
+        console.error(text,data || '');
+    }
+};
+let xwarn = function(text, data) {
+    if (window.location.host.indexOf('local') !== -1) {
+        /*var list = '';
+        for (var i=0; i < arguments.length; i++) {
+            list = list + arguments[i] + ' ';
+        }*/
+        console.warn(text,data || '');
     }
 };
 
--- sites/src/js/vendor.js
+++ sites/src/js/vendor.js
@@ -5823,13 +5823,32 @@ else for(var m,y="left"==this.option.position?o.inside?this.grid.getX()+1:this.g
  * Вывод в консоль необходимой инфы для отладки. На проде этой информации видно не будет
  * xlog('same text', vm.data)
  */
-var xlog = function(text, data) {
+let xlog = function(text, data) {
     if (window.location.host.indexOf('local') !== -1) {
         /*var list = '';
         for (var i=0; i < arguments.length; i++) {
             list = list + arguments[i] + ' ';
         }*/
         console.info(text,data || '');
+    }
+};
+
+let xerror = function(text, data) {
+    if (window.location.host.indexOf('local') !== -1) {
+        /*var list = '';
+        for (var i=0; i < arguments.length; i++) {
+            list = list + arguments[i] + ' ';
+        }*/
+        console.error(text,data || '');
+    }
+};
+let xwarn = function(text, data) {
+    if (window.location.host.indexOf('local') !== -1) {
+        /*var list = '';
+        for (var i=0; i < arguments.length; i++) {
+            list = list + arguments[i] + ' ';
+        }*/
+        console.warn(text,data || '');
     }
 };
 
