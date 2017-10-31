dac74d1c79fb55bbb00d0db95bec8eda67439c31
Ravy <smart.fz@mail.ru>
2017-10-31 19:07:04 +0300

    Поиск в ЛК

--- assets/partners/modules/modals/tpls/lossesModal/lossesModal.controller.js
+++ assets/partners/modules/modals/tpls/lossesModal/lossesModal.controller.js
@@ -13,6 +13,7 @@
         vm.id = $rootScope.modalData.id;
 
         vm.getContractorInfo = getContractorInfo;
+        vm.getSecondParticipantInfo = getSecondParticipantInfo;
 
         //////////////////////
         activate();
@@ -35,7 +36,7 @@
 
         // 968 //vm.id
         function getLossData(){
-            $http.get(config.api + '/losses/applications/item/968?token=' + vm.token)
+            $http.get(config.api + '/losses/applications/item/1036?token=' + vm.token)
                 .then(function(res){
                     vm.ld = res.data.response;
                     vm.ld.photos = [];
@@ -45,7 +46,11 @@
                             vm.ld.photos.push(f.path)
                         })
                     }
-                    xlog('vm.lossData',vm.ld)
+                    if (vm.ld.files.scene.length > 0) {
+                        vm.ld.files.scene.forEach(function(f){
+                            vm.ld.photos.push(f.path)
+                        })
+                    }
                 })
         }
 
@@ -69,6 +74,29 @@
                             '<dt>Основной</dt><dd>'+res.data.response[0].is_primary+'</dd></dl>' +
                             '</div>';
                         vm.contractor = $sce.trustAsHtml(html);
+                    }
+                })
+        }
+
+        function getSecondParticipantInfo(id) {
+            $http.get(config.api + '/contractors/' + id + '?token=' + vm.token)
+                .then(function(res){
+                    if (res.data.result) {
+                        let html = '<div class="popover-html">' +
+                            '<dl class="dl-horizontal">' +
+                            '<dt>#</dt><dd>'+res.data.response[0].id+'</dd></dl>' +
+                            '<dl class="dl-horizontal">' +
+                            '<dt>Ф.И.О.</dt><dd>'+res.data.response[0].name +'</dd></dl>' +
+                            '<dl class="dl-horizontal">' +
+                            '<dt>Д.р.</dt><dd>'+res.data.response[0].birth_date+'</dd></dl>' +
+                            '<dl class="dl-horizontal">' +
+                            '<dt>Пол</dt><dd>'+res.data.response[0].sex+'</dd></dl>' +
+                            '<dl class="dl-horizontal">' +
+                            '<dt>Юр.статус</dt><dd>'+res.data.response[0].is_juridical+'</dd></dl>' +
+                            '<dl class="dl-horizontal">' +
+                            '<dt>Основной</dt><dd>'+res.data.response[0].is_primary+'</dd></dl>' +
+                            '</div>';
+                        vm.second_participant = $sce.trustAsHtml(html);
                     }
                 })
         }
